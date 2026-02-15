b014154d4e7709fcdba9570dc846196526d2972a
Joaquin <joaquin.hinostroza593@gmail.com>
2026-02-15 09:54:23 -0600

    finished Menu and Playlist sort by Genre

--- U4BLab/src/Playlist.java
+++ U4BLab/src/Playlist.java
@@ -1,5 +1,53 @@
 import java.util.*;
 public class Playlist {
     private ArrayList<Song> songList;
-    
+    public Playlist(ArrayList<Song> list){
+        songList=list;
+    }
+    public String toString(){
+        String format = "%-30s %-21s %-27s %-5s %11s";
+        String result = String.format(format,"Title","Artist","Album","year","Genre")+"\n";
+        result+="--------------------------------------------------------------------------------------------------\n";
+        for(Song song:songList){
+            result+= song.toString()+"\n";
+        }
+        return result;
+    }
+    public String sortAToZ(){
+        ArrayList<Song> sortedList = new ArrayList<>();
+        return"in Progress";
+    }
+    public String sortZToA(){
+        ArrayList<Song> sortedList = new ArrayList<>();
+        return"in Progress";
+    }
+    public String searchByGenre(String genre){
+        ArrayList<Song> sortedList = new ArrayList<>();
+        for(Song song:songList){
+            if(song.getGenre().equals(genre)){
+                sortedList.add(song);
+            }
+        }
+        if(sortedList.size()>0) {
+            String format = "%-30s %-21s %-27s %-5s %11s";
+            String result = String.format(format, "Title", "Artist", "Album", "year", "Genre") + "\n";
+            result += "--------------------------------------------------------------\n";
+            for (Song song : sortedList) {
+                result += song.toString() + "\n";
+            }
+            return result;
+        }
+        else{
+            return "There are no Songs in the "+genre+" section";
+        }
+    }
+    public String sortByReleaseYearNewToOld(){
+        ArrayList<Song> sortedList = new ArrayList<>();
+        return"in Progress";
+    }
+    public String sortByReleaseYearOldToNew(){
+        ArrayList<Song> sortedList = new ArrayList<>();
+        return"in Progress";
+    }
+
 }
--- U4BLab/src/SpotifyTester.java
+++ U4BLab/src/SpotifyTester.java
@@ -1,2 +1,62 @@
+import java.util.*;
+import java.io.*;
 public class SpotifyTester {
+    public static void main(String[] args) throws FileNotFoundException{
+        Scanner songFile = new Scanner(new File("spotify_unique_years_artists.txt"));
+        Scanner input = new Scanner(System.in);
+        ArrayList<Song> songs = new ArrayList<>();
+        while(songFile.hasNextLine()){
+            String data = songFile.nextLine();
+            String[] splitData = data.split(",");
+            String title = splitData[0];
+            String artist = splitData[1];
+            String Album = splitData[2];
+            int duration = Integer.parseInt(splitData[3]);
+            int years = Integer.parseInt(splitData[4]);
+            String genre = splitData[5];
+            songs.add(new Song(title,artist,Album,years,genre,duration));
+        }
+        Playlist playlist = new Playlist(songs);
+        songFile.close();
+        int choice=0;
+        while(choice!=7) {
+            String menu = "==== Spotify Menu ====\n";
+            menu+="1 - Sort by artist (A->Z)\n" +"2 - Sort by artist (Z->A)";
+            menu+="\n3 - Sort by year (oldest -> newest)\n"+"4 - Sort by year (newest -> oldest)\n";
+            menu+="5 - Search by genre\n6 - Display all songs\n7 - Quit";
+            System.out.println(menu);
+            System.out.println("Please enter choice (1-7):");
+            try {
+                choice = input.nextInt();
+                if(choice==1){
+                    System.out.println(playlist.sortAToZ());
+                }
+                else if(choice==2){
+                    System.out.println(playlist.sortZToA());
+                }
+                else if(choice==3){
+                    System.out.println(playlist.sortByReleaseYearOldToNew());
+                }
+                else if(choice==4){
+                    System.out.println(playlist.sortByReleaseYearNewToOld());
+                }
+                else if(choice==5){
+                    input.nextLine();
+                    System.out.println("Please Input Genre:");
+                    String genre = input.nextLine();
+                    System.out.println(playlist.searchByGenre(genre));
+                }
+                else if(choice==6){
+                    System.out.println(playlist.toString());
+                }
+                else if(choice<1||choice>7){
+                    System.out.println("Please input a number in the range from 1-7:");
+                }
+            }catch (InputMismatchException e){
+                System.out.println("Invalid input. Please input a number 1-7:");
+                input.next();
+            }
+
+        }
+    }
 }
